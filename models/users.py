87177a73c28ecb71dcdd21e129c38c73dfe577f0
Svet-Tok <120042069+Svet-Tok@users.noreply.github.com>
2023-07-07 16:08:44 +1000

    Update users.py

--- models/users.py
+++ models/users.py
@@ -1,7 +1,9 @@
+from sqlalchemy.exc import SQLAlchemyError
+
 from models.base import Base
 
 import sqlalchemy as sq
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Session
 
 
 class Users(Base):
@@ -15,15 +17,6 @@ class Users(Base):
     candidates = relationship('Candidates', secondary='users_candidates', back_populates='users', cascade='all,delete')
 
 
-class UserCandidate(Base):
-    __tablename__ = 'users_candidates'
-
-    __table_args__ = (sq.PrimaryKeyConstraint('user_id', 'candidate_id'),)
-
-    user_id = sq.Column(sq.Integer, sq.ForeignKey('users.user_id'))
-    candidate_id = sq.Column(sq.Integer, sq.ForeignKey('candidates.candidate_id'))
-
-
 class Photos(Base):
     __tablename__ = 'photos'
 
@@ -34,8 +27,69 @@ class Photos(Base):
 
 
 class Candidates(Base):
-
     __tablename__ = 'candidates'
+
+    candidate_id = sq.Column(sq.Integer, primary_key=True)
+    first_name = sq.Column(sq.String(50))
+    last_name = sq.Column(sq.String(50))
+    vk_id = sq.Column(sq.String, nullable=False, unique=True)
+    users = relationship('Users', secondary='users_candidates', back_populates='candidates', cascade='all,delete')
+
+    def add_user(self):
+        """Метод добавления пользователя в бд."""
+
+        user = Users(
+            vk_id=self.user_id,
+            sex=self.sex,
+            age=self.age,
+            city=self.city
+        )
+        try:
+            self.session.add(user)
+            self.session.commit()
+        except SQLAlchemyError:
+            self.session.rollback()
+            return 'DB ERROR'
+
+    def add_photo(self):
+        """Метод добавления фотографии в бд."""
+
+        photo = Photos(
+            owner_id=self.owner_id,
+            like_count=self.like_count,
+            candidate_id=self.candidate_id
+
+        )
+
+        try:
+            self.session.add(photo)
+            self.session.commit()
+        except SQLAlchemyError:
+            self.session.rollback()
+            return 'DB ERROR'
+
+    def add_candidate(self):
+        """Метод добавления кандидата в бд."""
+
+        candidate = Candidates(
+            first_name=self.first_name,
+            last_name=self.last_name,
+            vk_id=self.vk_id
+        )
+
+        self.session.add(candidate)
+
+        return candidate
+
+
+def check_user(engine, user_id, vk_id ):
+    with Session(engine) as session:
+        from_bd = session.query(Users).filter(
+            Users.user_id == user_id,
+            Users.worksheet_id == vk_id
+        ).first()
+        return True if from_bd else False
+
 
     candidate_id = sq.Column(sq.Integer, primary_key=True)
     first_name = sq.Column(sq.String(50))
